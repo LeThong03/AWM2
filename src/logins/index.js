c484852ad8b52ff3bea691ae0ffe522389366045
DuyKs <unravengundam@gmai.com>
2024-03-27 01:45:33 +0700

    ErrorPage Complete

--- src/logins/index.js
+++ src/logins/index.js
@@ -1,6 +1,6 @@
 export { default as Login} from './login/Login';
 export { default as Regester} from './regester/Regester';
-export { default as notfound} from './regester/Regester';
+export { default as Error404} from './error404/Error404';
 
 
 
