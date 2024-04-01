375c60da0915454f79f0db16929d5517d004b25d
DuyKs <unravengundam@gmai.com>
2024-04-02 04:01:12 +0700

    Log

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 export { default as Login } from './login/Login';
 export { default as Register } from './register/Register';
-export { default as RegisterSuccess } from './register_Success/RegisterSuccess';
+export { default as RegisterSuccess } from './registerSuccess/RegisterSuccess';
 export { default as Error404 } from './notfound/Error404';
-
 
 /*export { default as LoginTest} from './loginTest/LoginTest';*/
