7af77074cd08fe22afa5c334a40c196a5b22cdd3
DuyKs <unravengundam@gmai.com>
2024-04-04 15:55:30 +0700

    Fix notFound

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 export { default as Login } from './login/Login';
 export { default as Register } from './register/Register';
 export { default as RegisterSuccess } from './registerSuccess/RegisterSuccess';
-export { default as Error404 } from './notfound/Error404';
+export { default as Error404 } from './notFound/Error404';
 export { default as Student } from './student/Student';
 
 /*export { default as LoginTest} from './loginTest/LoginTest';*/
